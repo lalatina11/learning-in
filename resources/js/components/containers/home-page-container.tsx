85eb680ed826e755cf7b6827ad247d18f6cefe45
lalatina11 <rahmadancandra@gmail.com>
2025-12-10 20:58:13 +0700

    feat: apply GravityStarsBackground

---
+++ resources/js/components/containers/home-page-container.tsx
@@ -0,0 +1,7 @@
+import { GravityStarsBackground } from '../animate-ui/components/backgrounds/gravity-stars';
+
+const HomePageContainer = () => {
+    return <GravityStarsBackground className="absolute inset-0 -z-10 flex items-center justify-center" />;
+};
+
+export default HomePageContainer;
