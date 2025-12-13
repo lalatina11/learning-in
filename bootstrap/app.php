ecf1f268690b4eaf3ca8866b2d1ba7431af1d297
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 18:06:21 +0700

    refactor: register all middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\AuthMiddleware;
+use App\Http\Middleware\CustomAuthMiddleware;
+use App\Http\Middleware\GuestOnlyMiddleware;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\StudentMiddleware;
+use App\Http\Middleware\TeacherMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -8,14 +14,21 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+        ]);
+        $middleware->alias([
+            'auth.middleware' => AuthMiddleware::class,
+            'guest.middleware' => GuestOnlyMiddleware::class,
+            'admin.middleware' => AdminMiddleware::class,
+            'teacher.middleware' => TeacherMiddleware::class,
+            'student.middleware' => StudentMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
