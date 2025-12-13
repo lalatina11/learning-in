f885340f5bb78b18e6df1f1911708c4d12a352f5
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 18:07:31 +0700

    feat: login form handle

--- resources/js/components/forms/login-form.tsx
+++ resources/js/components/forms/login-form.tsx
@@ -1,6 +1,6 @@
 import { loginSchema, LoginSchemaType } from '@/lib/form-schema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { usePage } from '@inertiajs/react';
+import { router, usePage } from '@inertiajs/react';
 import { Eye, EyeClosed } from 'lucide-react';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
@@ -9,10 +9,13 @@ import { RippleButton, RippleButtonRipples } from '../animate-ui/components/butt
 import { Field, FieldDescription, FieldError, FieldLabel } from '../ui/field';
 import { Input } from '../ui/input';
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from '../ui/input-group';
+import { Spinner } from '../ui/spinner';
 
 const LoginForm = () => {
     const { errors } = usePage().props;
+
     const [isShowPassword, setIsShowPassword] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const form = useForm({
         resolver: zodResolver(loginSchema),
         defaultValues: {
@@ -22,20 +25,37 @@ const LoginForm = () => {
     });
 
     const handleSubmit = async (body: LoginSchemaType) => {
-        console.log(body);
-
-        // router.post('/user');
-        toast.success('Berhasil Login', {
-            description: 'Anda sedang diarahkan menuju halaman dashboard!',
-            action: {
-                label: 'OK',
-                onClick: () => {},
+        router.post('login', body, {
+            onStart: () => {
+                setIsLoading(true);
+            },
+            onFinish: () => {
+                setIsLoading(false);
             },
         });
+        if (errors.server?.[0]) {
+            form.setError('root', { message: errors.server?.[0] || 'Terjadi Kesalahan' });
+            toast.error('Gagal login', {
+                description: errors.server?.[0] || 'Terjadi Kesalahan',
+                action: {
+                    label: 'OK',
+                    onClick: () => {},
+                },
+            });
+        } else {
+            toast.success('Berhasil Login', {
+                description: 'Anda sedang diarahkan menuju halaman dashboard!',
+                action: {
+                    label: 'OK',
+                    onClick: () => {},
+                },
+            });
+        }
     };
 
     return (
         <form onSubmit={form.handleSubmit(handleSubmit)} className="flex flex-col gap-4">
+            {form.formState.errors.root && <FieldError className="w-full text-center text-xs" errors={[form.formState.errors.root]} />}
             <Controller
                 name="master_number"
                 control={form.control}
@@ -74,8 +94,8 @@ const LoginForm = () => {
                     </Field>
                 )}
             />
-            <RippleButton>
-                Login
+            <RippleButton disabled={isLoading}>
+                {isLoading ? <Spinner /> : 'Login'}
                 <RippleButtonRipples />
             </RippleButton>
         </form>
