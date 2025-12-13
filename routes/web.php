3953276bd3d85e38b3ffd495bb7ebe641cd93d2d
lalatina11 <rahmadancandra@gmail.com>
2025-12-14 06:32:53 +0700

    feat: Route logout

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,19 @@ Route::middleware(['guest.middleware'])->group(function () {
     Route::get('/login', [AuthController::class, 'showAuthPage'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
-Route::middleware(['auth.middleware'])->prefix('/dashboard')->group(function () {
-    Route::middleware(['admin.middleware'])->prefix('/admin')->group(function () {
-        Route::get('/', [DashboardController::class, 'showAdminDashboard'])->name('dashboard.admin');
+
+Route::middleware(['auth.middleware'])->group(function () {
+    Route::prefix('/dashboard')->group(function () {
+
+        Route::middleware(['admin.middleware'])->prefix('/admin')->group(function () {
+            Route::get('/', [DashboardController::class, 'showAdminDashboard'])->name('dashboard.admin');
+        });
+        Route::middleware(['teacher.middleware'])->prefix('/teacher')->group(function () {
+            Route::get('/', [DashboardController::class, 'showTeacherDashboard'])->name('dashboard.teacher');
+        });
+        Route::middleware(['student.middleware'])->group(function () {
+            Route::get('/', [DashboardController::class, 'showStudentDashboard'])->name('dashboard.student');
+        });
     });
-    Route::middleware(['teacher.middleware'])->prefix('/teacher')->group(function () {
-        Route::get('/', [DashboardController::class, 'showTeacherDashboard'])->name('dashboard.teacher');
-    });
-    Route::middleware(['student.middleware'])->group(function () {
-        Route::get('/', [DashboardController::class, 'showStudentDashboard'])->name('dashboard.student');
-    });
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
 });
