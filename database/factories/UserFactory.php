90409182c5349c5c07b448e4b21ca08978489e00
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 14:52:38 +0700

    refactor: adjust user factory data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => fake()->firstName() . " " . fake()->lastName(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' =>  Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +36,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
