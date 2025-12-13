b0f0b279f1dca7acf5035afb4400438121123371
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 18:05:48 +0700

    feat: user role student only middleware

---
+++ app/Http/Middleware/StudentMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class StudentMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+        if (!$user) {
+            return redirect()->route('login');
+        }
+        if ($user->role !== "STUDENT") {
+            return redirect()->route('dashboard.' . Str::lower($user->role));
+        }
+        return $next($request);
+    }
+}
