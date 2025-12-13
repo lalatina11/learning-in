e967331ebc1c0b34bd6c8672ab689c38a9990e8a
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 18:04:44 +0700

    feat: rendering dashboard for each user role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    //
+    public function showAdminDashboard()
+    {
+        return Inertia::render("dashboard/admin/index",);
+    }
+    public function showTeacherDashboard()
+    {
+        return Inertia::render("dashboard/teacher/index",);
+    }
+    public function showStudentDashboard()
+    {
+        return Inertia::render("dashboard/index",);
+    }
 }
