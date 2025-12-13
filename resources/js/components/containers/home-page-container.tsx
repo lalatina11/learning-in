95aa7d478b4c1e160d944176868d792b2cde8a1a
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 14:54:18 +0700

    chore: HomePageContainer

--- resources/js/components/containers/home-page-container.tsx
+++ resources/js/components/containers/home-page-container.tsx
@@ -1,7 +1,22 @@
+import { ReactNode } from 'react';
 import { GravityStarsBackground } from '../animate-ui/components/backgrounds/gravity-stars';
+import Footer from '../footer';
+import ToasterProviderContainer from './toaster-provider-container';
 
-const HomePageContainer = () => {
-    return <GravityStarsBackground className="absolute inset-0 -z-10 flex items-center justify-center" />;
+interface Props {
+    children: ReactNode;
+}
+
+const HomePageContainer = ({ children }: Props) => {
+    return (
+        <ToasterProviderContainer>
+            <GravityStarsBackground className="absolute inset-0 -z-10 flex items-center justify-center" />
+            <div className="m-0">
+                {children}
+                <Footer />
+            </div>
+        </ToasterProviderContainer>
+    );
 };
 
 export default HomePageContainer;
