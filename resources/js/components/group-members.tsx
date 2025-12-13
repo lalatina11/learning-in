fa3c5631c047c035b807b261eedb642be82e9bca
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 09:25:28 +0700

    refactor: adjust slide using group members data

--- resources/js/components/group-members.tsx
+++ resources/js/components/group-members.tsx
@@ -1,12 +1,20 @@
 'use client';
 
+import { GroupMemberSlide } from '@/types';
 import { EmblaOptionsType } from 'embla-carousel';
 import { GroupMembersMotionCarousel } from './animate-ui/components/community/group-members-motion-carousel';
 
+export const SLIDES = [
+    { image: '/candra.png', name: 'Candra Rahmadan', nim: '19240731' },
+    { image: '/ires.jpeg', name: 'Ires Agil Karisma', nim: '19240730' },
+    { image: '/yoga.jpg', name: 'Yoga Rozan Pradana', nim: '19240730' },
+    { image: '/aga.jpg', name: 'Aga Panggih Yanuar Saputra', nim: '19240297' },
+    { image: '/unggul.PNG', name: 'Unggul Rahmat Pradana', nim: '19240675' },
+    { image: '', name: 'Augusta Catur Hasmoro', nim: '19241222' },
+] as Array<GroupMemberSlide>;
+
 const GroupMembers = () => {
     const OPTIONS: EmblaOptionsType = { loop: true };
-    const SLIDE_COUNT = 5;
-    const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
     return <GroupMembersMotionCarousel slides={SLIDES} options={OPTIONS} />;
 };
