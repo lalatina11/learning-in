8450b5c743751aae02c87d41965be6ad528ff127
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 14:53:52 +0700

    chore: footer

---
+++ resources/js/components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="mt-10 flex w-screen items-center justify-center bg-primary-foreground p-4">
+            <div className="flex flex-col items-center">
+                <span className="text-lg font-semibold">Learning IN</span>
+                <span className="text-sm">&copy; 2025 Learning IN. All rights reserved.</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
