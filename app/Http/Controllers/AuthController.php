14c5ba5395a70b3934a1c39255b75c8e52a9cd41
lalatina11 <rahmadancandra@gmail.com>
2025-12-13 18:04:27 +0700

    feat: handle login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AuthController extends Controller
@@ -25,33 +27,45 @@ class AuthController extends Controller
 
     public function showAuthPage()
     {
-        $this->cannotLogin();
-        return Inertia::render('auth');
+        // if ($redirect = $this->cannotLogin()) {
+        //     return $redirect;
+        // }
+        return Inertia::render('auth',);
     }
 
     public function login(Request $request)
     {
-        $this->cannotLogin();
+
+        // if ($redirect = $this->cannotLogin()) {
+        //     return $redirect;
+        // }
         $validated = $request->validate([
-            'identifier' => 'string|min:3',
+            'master_number' => 'string|min:8|max:16',
             'password' => 'string|min:8'
         ]);
-        $user = User::where('email', $validated['identifier'])->orWhere('name', $validated['identifier'])->first();
+        $user = User::where('master_number', $validated['master_number'])->first();
         if (!$user) {
-            // return 
+            return redirect()->back()->withErrors("Pengguna tidak terdaftar", 'server');
         }
+        $token = Auth::attempt($validated);
+        if (!$token) {
+            return redirect()->back()->withErrors("Password tidak valid", 'server');
+        }
+        if ($user->role === "ADMIN") {
+            return redirect()->route('dashboard.admin');
+        }
+        if ($user->role === "TEACHER") {
+            return redirect()->route('dashboard.teacher');
+        }
+        return redirect()->route('dashboard.student');
     }
     public function register(Request $request)
     {
-        $this->cannotLogin();
+        // $this->cannotLogin();
         $validated = $request->validate([
             'identifier' => 'string|min:3',
             'password' => 'string|min:8',
             'role' => 'required|in:TEACHER,STUDENT,ADMIN'
         ]);
-        $user = User::where('email', $validated['identifier'])->orWhere('name', $validated['identifier'])->first();
-        if (!$user) {
-            // return 
-        }
     }
 }
