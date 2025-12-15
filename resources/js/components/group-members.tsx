11850a6c1556a39817e833b8fe6c0252fb4a1b3f
lalatina11 <rahmadancandra@gmail.com>
2025-12-15 11:12:15 +0700

    refactor: change member sort

--- resources/js/components/group-members.tsx
+++ resources/js/components/group-members.tsx
@@ -5,12 +5,12 @@ import { EmblaOptionsType } from 'embla-carousel';
 import { GroupMembersMotionCarousel } from './animate-ui/components/community/group-members-motion-carousel';
 
 export const SLIDES = [
-    { image: '/candra.png', name: 'Candra Rahmadan', nim: '19240731' },
     { image: '/ires.jpeg', name: 'Ires Agil Karisma', nim: '19240730' },
+    { image: '/candra.png', name: 'Candra Rahmadan', nim: '19240731' },
     { image: '/yoga.jpg', name: 'Yoga Rozan Pradana', nim: '19240730' },
     { image: '/aga.jpg', name: 'Aga Panggih Yanuar Saputra', nim: '19240297' },
     { image: '/unggul.PNG', name: 'Unggul Rahmat Pradana', nim: '19240675' },
-    { image: '', name: 'Augusta Catur Hasmoro', nim: '19241222' },
+    { image: 'augusta.webp', name: 'Augusta Catur Hasmoro', nim: '19241222' },
 ] as Array<GroupMemberSlide>;
 
 const GroupMembers = () => {
