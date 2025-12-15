7f921934e08ab9331d64db4303afdee75882e4ff
lalatina11 <rahmadancandra@gmail.com>
2025-12-15 12:27:03 +0700

    feat: apply dashboard container page by wrapping admin dashboard page

--- resources/js/pages/dashboard/admin/index.tsx
+++ resources/js/pages/dashboard/admin/index.tsx
@@ -1,7 +1,13 @@
-const AdminDashboardIndexPage = (props: any) => {
-    console.log(props);
+import DashboardPageContainer from '@/components/containers/dashboard-page-container';
 
-    return <div>AdminDashboardIndexPage</div>;
+const AdminDashboardIndexPage = (props: any) => {
+    return (
+        <DashboardPageContainer>
+            <main className="flex h-full w-full items-center justify-center">
+                <span>Hello, selamat datang di admin dashboard!</span>
+            </main>
+        </DashboardPageContainer>
+    );
 };
 
 export default AdminDashboardIndexPage;
