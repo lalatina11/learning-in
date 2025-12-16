4ed6a29a51c159d93749f070bcec975805980bda
lalatina11 <rahmadancandra@gmail.com>
2025-12-16 10:59:38 +0700

    feat: create student_in_the_study_room migration for handling many to many relations table between users with student role, with studyroom table

---
+++ database/migrations/2025_12_16_031908_student_in_the_study_room_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_in_the_study_room', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('users', 'id')->onDelete('cascade');
+            $table->foreignId('study_room_id')->constrained('study_rooms', 'id')->onDelete('cascade');
+            $table->timestamps();
+            $table->unique(['student_id', 'study_room_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('the_study_room', function (Blueprint $table) {
+            //
+        });
+    }
+};
