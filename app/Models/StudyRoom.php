7f8462a8db91e8ade438d60c2ed479b779d66568
lalatina11 <rahmadancandra@gmail.com>
2025-12-16 10:58:42 +0700

    chore: create study_room model and migration

---
+++ app/Models/StudyRoom.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudyRoom extends Model
+{
+    protected $fillable = ['teacher_id', 'classroom_id'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+    public function classroom()
+    {
+        return $this->belongsTo(ClassRoom::class, 'classroom_id');
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class, 'student_in_the_study_room', 'study_room_id', 'student_id')->withTimestamps();
+    }
+
+    public function studentsCount()
+    {
+        return $this->students()->count();
+    }
+}
