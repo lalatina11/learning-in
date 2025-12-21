6bace3a4cc2bcd89b2eef9e50c02ac7fc964d6ab
lalatina11 <rahmadancandra@gmail.com>
2025-12-21 17:36:27 +0700

    chore: brand new school dashboard management page

---
+++ resources/js/pages/dashboard/admin/school.tsx
@@ -0,0 +1,11 @@
+import DashboardPageContainer from '@/components/containers/dashboard-page-container';
+
+const School = () => {
+    return (
+        <DashboardPageContainer>
+            <span>Hi</span>
+        </DashboardPageContainer>
+    );
+};
+
+export default School;
