3e18bf164a1035a9882ef63cc1fbff92787be0ed
lalatina11 <rahmadancandra@gmail.com>
2025-12-21 17:33:40 +0700

    feat: major seeder with sequence

---
+++ database/seeders/MajorSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Major;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MajorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Major::factory()->count(5)->sequence([
+            ['name' => "Bahasa Indonesia"],
+            ['name' => "Bahasa Inggris"],
+            ['name' => "Bahasa Jawa"],
+            ['name' => "Matematika"],
+            ['name' => "Informatika"],
+        ]);
+    }
+}
