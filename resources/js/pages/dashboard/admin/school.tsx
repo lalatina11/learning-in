e63a60b4cb5c56b9c3e57fefa157a96a4201a33c
lalatina11 <rahmadancandra@gmail.com>
2025-12-21 19:23:07 +0700

    feat: major crud

--- resources/js/pages/dashboard/admin/school.tsx
+++ resources/js/pages/dashboard/admin/school.tsx
@@ -1,9 +1,25 @@
+import MajorTable from '@/components/admin-components/major-table';
 import DashboardPageContainer from '@/components/containers/dashboard-page-container';
+import CreateOrUpdateMajorForm from '@/components/forms/create-or-update-or-delete-major-form';
+import { Button } from '@/components/ui/button';
+import { Major } from '@/types';
+import { Plus } from 'lucide-react';
 
-const School = () => {
+interface Props {
+    majors: Array<Major>;
+}
+
+const School = ({ majors }: Props) => {
     return (
         <DashboardPageContainer>
-            <span>Hi</span>
+            <div className="flex flex-col gap-2">
+                <CreateOrUpdateMajorForm type="create">
+                    <Button className="w-fit">
+                        <Plus /> Tambah Jurusan
+                    </Button>
+                </CreateOrUpdateMajorForm>
+                <MajorTable majors={majors} />
+            </div>
         </DashboardPageContainer>
     );
 };
