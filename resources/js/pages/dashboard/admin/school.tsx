ac3d3db59e84d32f78578199b0d877a4edd85080
lalatina11 <rahmadancandra@gmail.com>
2025-12-22 18:04:45 +0700

    feat: apply study room crud

--- resources/js/pages/dashboard/admin/school.tsx
+++ resources/js/pages/dashboard/admin/school.tsx
@@ -1,19 +1,22 @@
 import ClassRoomTable from '@/components/admin-components/class-room-table';
 import MajorTable from '@/components/admin-components/major-table';
+import StudyRoomTable from '@/components/admin-components/study-room-table';
 import DashboardPageContainer from '@/components/containers/dashboard-page-container';
 import ClassRoomForm from '@/components/forms/class-room-form';
 import MajorForm from '@/components/forms/major-form';
+import StudyRoomForm from '@/components/forms/study-room-form';
 import { Button } from '@/components/ui/button';
-import { ClassRoomWithMajor, Major } from '@/types';
+import { ClassRoomWithMajor, Major, StudyRoomWithClassRoomAndTeacher } from '@/types';
 import { Plus } from 'lucide-react';
 
 interface Props {
     majors: Array<Major>;
     classRooms: Array<ClassRoomWithMajor>;
+    studyRooms: Array<StudyRoomWithClassRoomAndTeacher>;
 }
 
-const School = ({ majors, classRooms }: Props) => {
-    console.log({ classRooms, majors });
+const School = ({ majors, classRooms, studyRooms }: Props) => {
+    console.log({ studyRooms });
 
     return (
         <DashboardPageContainer>
@@ -32,6 +35,16 @@ const School = ({ majors, classRooms }: Props) => {
                     </Button>
                 </ClassRoomForm>
                 <ClassRoomTable classRooms={classRooms} />
+            </div>
+            <div className="flex flex-col gap-2">
+                <StudyRoomForm type="create">
+                    <Button className="w-fit">
+                        <Plus />
+                        <span className="hidden md:inline">Kegiatan Mengajar</span>
+                        <span className="inline md:hidden">KBM</span>
+                    </Button>
+                </StudyRoomForm>
+                <StudyRoomTable studyRooms={studyRooms} />
             </div>
         </DashboardPageContainer>
     );
