b14e65a65df007203a849839f10be508e6ae26eb
lalatina11 <rahmadancandra@gmail.com>
2025-12-25 17:06:14 +0700

    feat: apply learning subject crud

--- resources/js/pages/dashboard/admin/school.tsx
+++ resources/js/pages/dashboard/admin/school.tsx
@@ -1,21 +1,24 @@
 import ClassRoomTable from '@/components/admin-components/class-room-table';
+import LearningSubjectTable from '@/components/admin-components/learning-subject-table';
 import MajorTable from '@/components/admin-components/major-table';
 import StudyRoomTable from '@/components/admin-components/study-room-table';
 import DashboardPageContainer from '@/components/containers/dashboard-page-container';
 import ClassRoomForm from '@/components/forms/class-room-form';
+import LearningSubjectForm from '@/components/forms/learning-subject-form';
 import MajorForm from '@/components/forms/major-form';
 import StudyRoomForm from '@/components/forms/study-room-form';
 import { Button } from '@/components/ui/button';
-import { ClassRoomWithMajor, Major, StudyRoomWithClassRoomAndTeacher } from '@/types';
+import { ClassRoomWithMajor, LearningSubject, Major, StudyRoomWithClassRoomAndTeacher } from '@/types';
 import { Plus } from 'lucide-react';
 
 interface Props {
     majors: Array<Major>;
     classRooms: Array<ClassRoomWithMajor>;
     studyRooms: Array<StudyRoomWithClassRoomAndTeacher>;
+    learningSubjects: Array<LearningSubject>;
 }
 
-const School = ({ majors, classRooms, studyRooms }: Props) => {
+const School = ({ majors, classRooms, studyRooms, learningSubjects }: Props) => {
     return (
         <DashboardPageContainer>
             <div className="flex flex-col gap-2">
@@ -35,10 +38,20 @@ const School = ({ majors, classRooms, studyRooms }: Props) => {
                 <ClassRoomTable classRooms={classRooms} />
             </div>
             <div className="flex flex-col gap-2">
+                <LearningSubjectForm type="create">
+                    <Button className="w-fit">
+                        <Plus />
+                        <span className="hidden md:inline">Tambah Mata Pelajaran</span>
+                        <span className="inline md:hidden">Mapel</span>
+                    </Button>
+                </LearningSubjectForm>
+                <LearningSubjectTable learningSubjects={learningSubjects} />
+            </div>
+            <div className="flex flex-col gap-2">
                 <StudyRoomForm type="create">
                     <Button className="w-fit">
                         <Plus />
-                        <span className="hidden md:inline">Kegiatan Mengajar</span>
+                        <span className="hidden md:inline">Tambah Kegiatan Mengajar</span>
                         <span className="inline md:hidden">KBM</span>
                     </Button>
                 </StudyRoomForm>
