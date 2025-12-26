890eb958b00389c0c6f975b8b5c73bd4ae003ae2
lalatina11 <rahmadancandra@gmail.com>
2025-12-26 19:49:53 +0700

    refactor: move colon position

--- resources/js/pages/dashboard/admin/study-room-detail.tsx
+++ resources/js/pages/dashboard/admin/study-room-detail.tsx
@@ -27,28 +27,28 @@ const StudyRoomDetail = ({ studyRoom }: Props) => {
                         <CardTitle>Detail KBM</CardTitle>
                         <CardDescription className="flex flex-col gap-1">
                             <span className="flex flex-1">
-                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Mapel:</span>
-                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">{studyRoom.learning_subject.name}</span>
+                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Mapel</span>
+                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">: {studyRoom.learning_subject.name}</span>
                             </span>
                             <span className="flex flex-1">
-                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Tingkatan:</span>
-                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">{studyRoom.classroom.grade}</span>
+                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Tingkatan</span>
+                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">: {studyRoom.classroom.grade}</span>
                             </span>
                             <span className="flex flex-1">
-                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Jurusan:</span>
-                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">{studyRoom.classroom.major.name}</span>
+                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Jurusan</span>
+                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">: {studyRoom.classroom.major.name}</span>
                             </span>
                             <span className="flex flex-1">
-                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">NIP Guru:</span>
-                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">{studyRoom.teacher.master_number}</span>
+                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">NIP Guru</span>
+                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">: {studyRoom.teacher.master_number}</span>
                             </span>
                             <span className="flex flex-1">
-                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Nama Guru:</span>
-                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">{studyRoom.teacher.name}</span>
+                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Nama Guru</span>
+                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">: {studyRoom.teacher.name}</span>
                             </span>
                             <span className="flex flex-1">
-                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Jumlah Siswa:</span>
-                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">{studyRoom.students.length}</span>
+                                <span className="flex-1/3 sm:flex-1/2 md:flex-2/8 lg:flex-1/8">Jumlah Siswa</span>
+                                <span className="flex-2/3 sm:flex-1/2 md:flex-6/8 lg:flex-7/8">: {studyRoom.students.length}</span>
                             </span>
                         </CardDescription>
                     </CardHeader>
