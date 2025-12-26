18772930b30ab6bb9319a02d4b9714766719c589
lalatina11 <rahmadancandra@gmail.com>
2025-12-26 19:47:55 +0700

    feat: read data for teacher dashboard

--- app/Http/Controllers/TeacherDashboardController.php
+++ app/Http/Controllers/TeacherDashboardController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StudyRoom;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class TeacherDashboardController extends Controller
 {
-    public function showTeacherDashboard()
+    public function showDashboard()
     {
-        return Inertia::render("dashboard/teacher/index",);
+        return Inertia::render("dashboard/teacher/index", );
+    }
+    public function showLearningDashboard()
+    {
+        $studyRooms = StudyRoom::where('teacher_id', request()->user()->id)->with('classroom')->with('learning_subject')->get();
+        return Inertia::render("dashboard/teacher/learning/index", compact("studyRooms"));
+    }
+    public function showLearningDashboardDetails($id)
+    {
+        $studyRoom = StudyRoom::where('teacher_id', request()->user()->id)->with('classroom')->with('teacher')->with('students')->with('learning_subject')->findOrFail($id);
+        if (!$studyRoom) {
+            return redirect()->route('');
+        }
+        return Inertia::render("dashboard/teacher/learning/details", compact("studyRoom"));
     }
 }
