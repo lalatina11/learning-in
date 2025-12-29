e8ae0dc699797ced6a77784f50c73f4ba40d3aa0
lalatina11 <rahmadancandra@gmail.com>
2025-12-29 08:10:17 +0700

    feat: classroom factory

--- app/Models/ClassRoom.php
+++ app/Models/ClassRoom.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ClassRoom extends Model
 {
+    use HasFactory;
     protected $fillable = ['grade', 'major_id'];
 
     protected $casts = ['grade' => 'string'];
