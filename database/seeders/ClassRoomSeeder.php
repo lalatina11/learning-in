ddeb92219680b470271736b815993ed3b5955e24
lalatina11 <rahmadancandra@gmail.com>
2025-12-29 08:10:26 +0700

    feat: classroom seeder

---
+++ database/seeders/ClassRoomSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ClassRoom;
+use App\Models\Major;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassRoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $major1 = Major::first();
+        $major2 = Major::skip(1)->first();
+        ClassRoom::factory()->count(6)->sequence(
+            ['grade' => 'X', 'major_id' => $major1->id],
+            ['grade' => 'XI', 'major_id' => $major1->id],
+            ['grade' => 'XII', 'major_id' => $major1->id],
+            ['grade' => 'X', 'major_id' => $major2->id],
+            ['grade' => 'XI', 'major_id' => $major2->id],
+            ['grade' => 'XII', 'major_id' => $major2->id],
+        )->create();
+    }
+}
