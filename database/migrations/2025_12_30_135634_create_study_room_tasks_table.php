61c133e8b1fc5f38456ee32bb2328dd7902126e3
lalatina11 <rahmadancandra@gmail.com>
2025-12-30 21:02:56 +0700

    feat: Study room tasks migration

---
+++ database/migrations/2025_12_30_135634_create_study_room_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('study_room_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('study_room_id')->constrained('study_rooms', 'id')->cascadeOnDelete();
+            $table->string('description');
+            $table->string('task');
+            $table->string('task_storage_url');
+            $table->boolean('is_closed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('study_room_tasks');
+    }
+};
