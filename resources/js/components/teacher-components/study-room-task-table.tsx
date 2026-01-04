7ea3f24ba26762b658fd293bd7cbcfe4c0e4e43f
lalatina11 <rahmadancandra@gmail.com>
2026-01-04 21:48:50 +0700

    feat: add more actions in StudyRoomTaskTable

--- resources/js/components/teacher-components/study-room-task-table.tsx
+++ resources/js/components/teacher-components/study-room-task-table.tsx
@@ -1,7 +1,7 @@
 import { Auth, StudyRoomModule, StudyRoomTask, StudyRoomWithClassRoomAndTeacherAndMajorAndLearningSubjectAndStudents } from '@/types';
 import { PageProps } from '@/types/page-props';
 import { usePage } from '@inertiajs/react';
-import { Copy, Edit, Lock, LockOpen, Trash } from 'lucide-react';
+import { Copy, Download, Edit, Lock, LockOpen, Trash, UploadIcon } from 'lucide-react';
 import { toast } from 'sonner';
 import {} from '../animate-ui/components/radix/dropdown-menu';
 import StudyRoomTaskForm from '../forms/study-room-task-form';
@@ -14,19 +14,35 @@ export interface StudyRoom extends StudyRoomWithClassRoomAndTeacherAndMajorAndLe
     tasks: Array<StudyRoomTask>;
 }
 
+interface DownloadButtonProps {
+    url: string;
+    index: number;
+}
+
 const StudyRoomTaskTable = () => {
     const { studyRoom, auth } = usePage().props as PageProps & { studyRoom: StudyRoom; auth: Auth };
 
     const { tasks } = studyRoom;
 
+    const isTeacher = auth.user.role === 'TEACHER';
+
+    function handleTaskDownload(props: DownloadButtonProps) {
+        const link = document.createElement('a');
+        link.href = props.url;
+        link.download = `Tugas-${studyRoom.classroom.grade}-${studyRoom.classroom.major.name}-${studyRoom.learning_subject.name}-${props.index}`;
+        link.target = '_blank';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return (
         <Table className="w-full">
-            <TableCaption>Berhasil mendapatkan semua data Penugasan</TableCaption>
+            <TableCaption>Semua data Module KBM sudah dimuat.</TableCaption>
             <TableHeader>
                 <TableRow>
                     <TableHead className="w-[100px]">#</TableHead>
                     <TableHead>Deskripsi</TableHead>
-                    <TableHead>Url</TableHead>
                     <TableHead>Status</TableHead>
                     <TableHead className="text-center">Aksi</TableHead>
                 </TableRow>
@@ -37,31 +53,33 @@ const StudyRoomTaskTable = () => {
                         <TableCell className="font-medium">{index + 1}</TableCell>
                         <TableCell>{task.description}</TableCell>
                         <TableCell>
-                            <span className="max-w-[300px] truncate">{task.url.slice(37)}</span>
-                        </TableCell>
-                        <TableCell>
                             <Badge variant={task.is_closed ? 'destructive' : 'default'}>{task.is_closed ? 'Ditutup' : 'Dibuka'}</Badge>
                         </TableCell>
                         <TableCell>
-                            <div className="flex items-center gap-1">
-                                <StudyRoomTaskForm type="update" task={task}>
-                                    <Button className="flex-1">
-                                        <Edit />
-                                        <span className="hidden md:inline">Update</span>
-                                    </Button>
-                                </StudyRoomTaskForm>
-                                <StudyRoomTaskForm type="delete" task={task}>
-                                    <Button className="flex-1" variant={'destructive'}>
-                                        <Trash />
-                                        <span className="hidden md:inline">Delete</span>
-                                    </Button>
-                                </StudyRoomTaskForm>
-                                <StudyRoomTaskForm type="switchClosed" task={task}>
-                                    <Button className="flex-1" variant={task.is_closed ? 'default' : 'destructive'}>
-                                        {task.is_closed ? <LockOpen /> : <Lock />}
-                                        <span className="hidden md:inline">{task.is_closed ? 'Buka' : 'Tutup'}</span>
-                                    </Button>
-                                </StudyRoomTaskForm>
+                            <div className="flex items-center justify-center gap-1">
+                                {isTeacher && (
+                                    <>
+                                        <StudyRoomTaskForm type="update" task={task}>
+                                            <Button className="flex-1">
+                                                <Edit />
+                                                <span className="hidden md:inline">Update</span>
+                                            </Button>
+                                        </StudyRoomTaskForm>
+
+                                        <StudyRoomTaskForm type="delete" task={task}>
+                                            <Button className="flex-1" variant={'destructive'}>
+                                                <Trash />
+                                                <span className="hidden md:inline">Delete</span>
+                                            </Button>
+                                        </StudyRoomTaskForm>
+                                        <StudyRoomTaskForm type="switchClosed" task={task}>
+                                            <Button className="flex-1" variant={task.is_closed ? 'default' : 'destructive'}>
+                                                {task.is_closed ? <LockOpen /> : <Lock />}
+                                                <span className="hidden md:inline">{task.is_closed ? 'Buka' : 'Tutup'}</span>
+                                            </Button>
+                                        </StudyRoomTaskForm>
+                                    </>
+                                )}
                                 <Button
                                     onClick={() => {
                                         navigator.clipboard.writeText(task.url);
@@ -71,6 +89,29 @@ const StudyRoomTaskTable = () => {
                                     <Copy />
                                     <span className="hidden md:inline">Copy Url</span>
                                 </Button>
+                                {!isTeacher && (
+                                    <>
+                                        <Button
+                                            onClick={() =>
+                                                handleTaskDownload({
+                                                    url: task.url,
+                                                    index: index + 1,
+                                                })
+                                            }
+                                        >
+                                            <Download />
+                                            <span className="hidden md:inline">Download</span>
+                                        </Button>
+                                        {!task.is_closed && (
+                                            <StudyRoomTaskForm type="submission" task={task}>
+                                                <Button>
+                                                    <UploadIcon />
+                                                    <span className="hidden md:inline">Kerjakan</span>
+                                                </Button>
+                                            </StudyRoomTaskForm>
+                                        )}
+                                    </>
+                                )}
                             </div>
                         </TableCell>
                     </TableRow>
